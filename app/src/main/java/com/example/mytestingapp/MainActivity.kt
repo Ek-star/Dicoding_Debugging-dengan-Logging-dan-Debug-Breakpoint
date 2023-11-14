35974d09219d8c01bfd15c62b8299367ced759a5
Ek-star <totoekalma@gmail.com>
2023-11-14 12:00:34 +0700

    First Commit

---
+++ app/src/main/java/com/example/mytestingapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mytestingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // Ketika mengembangkan aplikasi Android, tentu salah satu hal yang paling menantang adalah saat menemukan bug ketika aplikasi dijalankan. Memecahkan sebuah bug di platform android bukanlah hal yang mudah. Ada beragam spesifikasi perangkat Android yang beredar di pasar. Aplikasi kita bisa jadi berjalan lancar di satu peranti. Namun, bermasalah di peranti yang berbeda. Oleh karena itu, kita perlu mengetahui beberapa cara untuk melakukan Debugging. Proses tersebut mengharuskan kita melakukan proses identifikasi bug atau masalah yang muncul dan mencari solusinya.
+}
